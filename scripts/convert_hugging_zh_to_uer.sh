667fba6f6f2ad419662abd6e375da0264a151bb6
LeoWood <463861936@qq.com>
2020-04-30 21:22:40 +0800

    .

--- scripts/convert_hugging_zh_to_uer.sh
+++ scripts/convert_hugging_zh_to_uer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python convert_bert_from_huggingface_to_uer.py \
 --input_model_path ../models/bert-base-chinese-pytorch_model.bin \
---output_model_path ../models/google_zh_model.bin \
+--output_model_path ../models/google_zh_model.bin
