207571d804f0e0a34959191b83389b63bb915965
LeoWood <463861936@qq.com>
2020-05-02 16:43:31 +0800

    .

--- pretrain_R.sh
+++ pretrain_R.sh
@@ -2,8 +2,8 @@
 python pretrain.py \
 --dataset_path cscd_r.pt \
 --vocab_path models/google_zh_vocab.txt \
---pretrained_model_path models/google_zh_model.bin \
---output_model_path models/cscd_R_based_on_google_zh.bin  \
+--pretrained_model_path models/cscd_R_based_on_google_zh-100000.bin \
+--output_model_path models/cscd_R_based_on_google_zh_100000.bin  \
 --world_size 2 \
 --gpu_ranks 0 1 \
 --total_steps 100000 \
