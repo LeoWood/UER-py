53c53c73afbabf2e3372f400536423d63e7fb325
LeoWood <463861936@qq.com>
2020-05-04 20:21:54 +0800

    .

--- pre_process_test.sh
+++ pre_process_test.sh
@@ -3,5 +3,5 @@ python preprocess.py \
 --corpus_path corpora/book_review_bert.txt \
 --vocab_path models/google_zh_vocab.txt \
 --dataset_path dataset/book_review_bert.pt \
---processes_num 1 \
+--processes_num 3 \
 --target csci_mlm
