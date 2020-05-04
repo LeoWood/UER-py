4a4c2efe4d6c8337eec6bf4a37f44bb2ed19177d
LeoWood <463861936@qq.com>
2020-05-04 22:06:26 +0800

    .

--- preprocess_test.bat
+++ preprocess_test.bat
@@ -1,1 +1,1 @@
-python preprocess.py --corpus_path corpora/book_review_bert.txt --vocab_path models/google_zh_vocab.txt --dataset_path datasets/book_review_bert.pt --processes_num 16 --target csci_mlm
+E:/Anaconda3/envs/hanlp/python preprocess.py --corpus_path corpora/book_review_bert.txt --vocab_path models/google_zh_vocab.txt --dataset_path datasets/book_review_bert.pt --processes_num 16 --target csci_mlm
