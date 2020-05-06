015e1bab2828c0b96da6c923b49b1297cbdd5d60
LeoWood <463861936@qq.com>
2020-05-06 20:13:36 +0800

    .

--- uer/utils/extract_medical_terms.py
+++ uer/utils/extract_medical_terms.py
@@ -78,5 +78,5 @@ if __name__ == '__main__':
     # print('去掉HowNet:',len(final_terms))
 
     with open('medical_terms.txt','w',encoding='utf-8') as f:
-        [f.write(term + '\n') for term in final_terms]
+        [f.write(term + '\n') for term in final_terms if len(term) > 1]
 
