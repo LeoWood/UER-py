f5e7aafaba4a68ba2157dae25cda5406ba3150e7
LeoWood <463861936@qq.com>
2020-05-07 16:30:26 +0800

    .

--- pretrain_R_csci_mlm_no_pos.bat
+++ pretrain_R_csci_mlm_no_pos.bat
@@ -4,8 +4,8 @@ python pretrain.py ^
 --pretrained_model_path models/google_zh_model.bin ^
 --output_model_path models/cscd_R_csci_mlm_no_pos_based_on_google_zh.bin  ^
 --output_log_path models/cscd_R_csci_mlm_no_pos.csv  ^
---world_size 2 ^
---gpu_ranks 0 1 ^
+--world_size 1 ^
+--gpu_ranks 0 ^
 --total_steps 500000 ^
 --save_checkpoint_steps 10000 ^
 --encoder bert ^
