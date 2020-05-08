4d652e6e79ce6b4997469358f6ff6636ade9e18d
LeoWood <463861936@qq.com>
2020-05-08 11:55:08 +0800

    .

---
+++ ner_amttl_cscd_mlm.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+python run_ner.py \
+--pretrained_model_path models/cscd_R_based_on_google_zh_600000_best.bin \
+--vocab_path models/google_zh_vocab.txt \
+--output_model_path ./models/ner_amttl_bert_cscd_mlm.bin \
+--train_path datasets/amttl/train.tsv \
+--dev_path datasets/amttl/dev.tsv \
+--test_path datasets/amttl/test.tsv \
+--embedding bert \
+--encoder bert \
+--learning_rate 2e-5 \
+--warmup 0.1 \
+--epochs_num 3 \
+--seq_length 128 \
+--batch_size 32 \
+--report_steps 50
