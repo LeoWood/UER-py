9bed042645424b63af54d0086f2a64d5e320dc5e
LeoWood <463861936@qq.com>
2020-05-08 11:09:48 +0800

    .

---
+++ ner_amttl.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+python3 run_ner.py \
+--pretrained_model_path models/book_review_mlm_model.bin \
+--vocab_path models/google_zh_vocab.txt \
+--output_model_path ./models/ner_amttl_bert_base.bin \
+--train_path datasets/amttl/train.tsv \
+--dev_path datasets/amttl/dev.tsv \
+--test_path datasets/amttl/test.tsv \
+--embedding bert\
+--encoder bert \
+--learning_rate 2e-5\
+--warmup 0.1 \
+--epochs_num 3 \
+--batch_size 32 \
+--report_steps 50
