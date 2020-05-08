2ebda530b049324e301276cdbb21a66a0735b18b
LeoWood <463861936@qq.com>
2020-05-08 13:54:52 +0800

    .

--- cla_csl.sh
+++ cla_csl.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
-python3 run_classifier.py \
---pretrained_model_path models/book_review_mlm_model.bin \
+python run_classifier.py \
+--pretrained_model_path models/google_zh_model.bin \
 --vocab_path models/google_zh_vocab.txt \
 --output_model_path ./models/cla_csl_bert_base.bin \
 --train_path datasets/cls/train.tsv \
 --dev_path datasets/cls/dev.tsv \
 --test_path datasets/cls/test.tsv \
---epochs_num 3 \
---batch_size 32 \
---encoder bert
+--embedding bert \
+--encoder bert \
+--learning_rate 1e-5 \
+--warmup 0.1 \
+--epochs_num 5 \
+--seq_length 256 \
+--batch_size 10 \
+--report_steps 100
