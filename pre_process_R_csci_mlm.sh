0881e1067a8cd4d33334d5b2e0b50fa757e334c9
LeoWood <463861936@qq.com>
2020-05-12 23:15:22 +0800

    .

--- pre_process_R_csci_mlm.sh
+++ pre_process_R_csci_mlm.sh
@@ -3,5 +3,6 @@ python preprocess.py \
 --corpus_path corpora/R.txt \
 --vocab_path models/google_zh_vocab.txt \
 --dataset_path cscd_r_csci_mlm.pt \
---processes_num 8 \
---target csci_mlm
+--processes_num 4 \
+--target csci_mlm \
+--add_pos 1
