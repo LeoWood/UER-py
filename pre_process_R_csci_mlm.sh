606a66af7ee8314b5d99d4e8a80d17a0e2053186
LeoWood <463861936@qq.com>
2020-05-13 16:09:33 +0800

    .

--- pre_process_R_csci_mlm.sh
+++ pre_process_R_csci_mlm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 export PYTHONUNBUFFERED=1
 python preprocess.py \
---corpus_path corpora/R_test.txt \
+--corpus_path corpora/R.txt \
 --vocab_path models/google_zh_vocab.txt \
 --dataset_path cscd_r_csci_mlm.pt \
 --processes_num 4 \
