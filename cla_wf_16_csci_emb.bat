b31953309d4f73f8326948df9d5626e35005e6b7
LeoWood <463861936@qq.com>
2020-05-15 12:29:50 +0800

    .

---
+++ cla_wf_16_csci_emb.bat
@@ -0,0 +1,17 @@
+python run_classifier.py ^
+--pretrained_model_path models/google_zh_model.bin ^
+--vocab_path models/google_zh_vocab.txt ^
+--output_model_path ./models/cla_wf_16_bert_base.bin ^
+--train_path datasets/wanfang_16000/train.tsv ^
+--dev_path datasets/wanfang_16000/dev.tsv ^
+--test_path datasets/wanfang_16000/test.tsv ^
+--embedding cscibert ^
+--encoder bert ^
+--learning_rate 2e-5 ^
+--warmup 0.1 ^
+--epochs_num 3 ^
+--seq_length 400 ^
+--batch_size 5 ^
+--report_steps 100 ^
+--gpu_rank 0 ^
+--add_pos 1
