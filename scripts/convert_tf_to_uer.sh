d46fbef15b4ec396507f894af89fabf5a59a88c4
LeoWood <463861936@qq.com>
2020-05-18 10:05:47 +0800

    .

---
+++ scripts/convert_tf_to_uer.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+python convert_bert_from_huggingface_to_uer.py \
+--input_model_path ../models/chinese_wwm_ext_L-12_H-768_A-12/bert_model.ckpt \
+--output_model_path ../models/bert_wwm_ext_model.bin
