d62075c091fb081d92afee956e0ee7491dcd781e
LeoWood <463861936@qq.com>
2020-05-19 10:06:15 +0800

    .

--- ner_ccks_pos_term_wwm.sh
+++ ner_ccks_pos_term_wwm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python run_ner_csci_emb.py \
---pretrained_model_path models/bert-base-chinese-pytorch_model.bin \
+--pretrained_model_path models/bert_wwm_ext_model.bin \
 --vocab_path models/google_zh_vocab.txt \
 --output_model_path ./models/ner_ccks_bert_wwm.bin \
 --train_path datasets/ccks/train.tsv \
@@ -20,7 +20,7 @@ python run_ner_csci_emb.py \
 --add_term 0
 
 python run_ner_csci_emb.py \
---pretrained_model_path models/bert-base-chinese-pytorch_model.bin \
+--pretrained_model_path models/bert_wwm_ext_model.bin \
 --vocab_path models/google_zh_vocab.txt \
 --output_model_path ./models/ner_ccks_bert_wwm.bin \
 --train_path datasets/ccks/train.tsv \
@@ -40,7 +40,7 @@ python run_ner_csci_emb.py \
 --add_term 0
 
 python run_ner_csci_emb.py \
---pretrained_model_path models/bert-base-chinese-pytorch_model.bin \
+--pretrained_model_path models/bert_wwm_ext_model.bin \
 --vocab_path models/google_zh_vocab.txt \
 --output_model_path ./models/ner_ccks_bert_wwm.bin \
 --train_path datasets/ccks/train.tsv \
@@ -60,7 +60,88 @@ python run_ner_csci_emb.py \
 --add_term 1
 
 python run_ner_csci_emb.py \
---pretrained_model_path models/bert-base-chinese-pytorch_model.bin \
+--pretrained_model_path models/bert_wwm_ext_model.bin \
+--vocab_path models/google_zh_vocab.txt \
+--output_model_path ./models/ner_ccks_bert_wwm.bin \
+--train_path datasets/ccks/train.tsv \
+--dev_path datasets/ccks/dev.tsv \
+--test_path datasets/ccks/test.tsv \
+--log_path ./models/ner_ccsk_pos_term_bert_wwm.log \
+--embedding cscibert \
+--encoder bert \
+--learning_rate 2e-5 \
+--warmup 0.1 \
+--epochs_num 8 \
+--seq_length 128 \
+--batch_size 16 \
+--report_steps 50 \
+--gpu_rank 0 \
+--add_pos 1 \
+--add_term 1
+
+
+python run_ner_csci_emb.py \
+--pretrained_model_path models/roberta_wwm_ext_model.bin \
+--vocab_path models/google_zh_vocab.txt \
+--output_model_path ./models/ner_ccks_bert_wwm.bin \
+--train_path datasets/ccks/train.tsv \
+--dev_path datasets/ccks/dev.tsv \
+--test_path datasets/ccks/test.tsv \
+--log_path ./models/ner_ccsk_pos_bert_wwm.log \
+--embedding cscibert \
+--encoder bert \
+--learning_rate 2e-5 \
+--warmup 0.1 \
+--epochs_num 8 \
+--seq_length 128 \
+--batch_size 16 \
+--report_steps 50 \
+--gpu_rank 0 \
+--add_pos 0 \
+--add_term 0
+
+python run_ner_csci_emb.py \
+--pretrained_model_path models/roberta_wwm_ext_model.bin \
+--vocab_path models/google_zh_vocab.txt \
+--output_model_path ./models/ner_ccks_bert_wwm.bin \
+--train_path datasets/ccks/train.tsv \
+--dev_path datasets/ccks/dev.tsv \
+--test_path datasets/ccks/test.tsv \
+--log_path ./models/ner_ccsk_pos_bert_wwm.log \
+--embedding cscibert \
+--encoder bert \
+--learning_rate 2e-5 \
+--warmup 0.1 \
+--epochs_num 8 \
+--seq_length 128 \
+--batch_size 16 \
+--report_steps 50 \
+--gpu_rank 0 \
+--add_pos 1 \
+--add_term 0
+
+python run_ner_csci_emb.py \
+--pretrained_model_path models/roberta_wwm_ext_model.bin \
+--vocab_path models/google_zh_vocab.txt \
+--output_model_path ./models/ner_ccks_bert_wwm.bin \
+--train_path datasets/ccks/train.tsv \
+--dev_path datasets/ccks/dev.tsv \
+--test_path datasets/ccks/test.tsv \
+--log_path ./models/ner_ccsk_term_bert_wwm.log \
+--embedding cscibert \
+--encoder bert \
+--learning_rate 2e-5 \
+--warmup 0.1 \
+--epochs_num 8 \
+--seq_length 128 \
+--batch_size 16 \
+--report_steps 50 \
+--gpu_rank 0 \
+--add_pos 0 \
+--add_term 1
+
+python run_ner_csci_emb.py \
+--pretrained_model_path models/roberta_wwm_ext_model.bin \
 --vocab_path models/google_zh_vocab.txt \
 --output_model_path ./models/ner_ccks_bert_wwm.bin \
 --train_path datasets/ccks/train.tsv \
--- scripts/convert_tf_to_uer.sh
+++ scripts/convert_tf_to_uer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python convert_bert_from_google_to_uer.py \
---input_model_path /home/leo/lh/Projects/UER-py/models/chinese_wwm_ext_L-12_H-768_A-12/bert_model.ckpt \
---output_model_path /home/leo/lh/Projects/UER-py/models/bert_wwm_ext_model.bin
+--input_model_path /home/leo/lh/Projects/UER-py/models/chinese_roberta_wwm_ext_L-12_H-768_A-12/bert_model.ckpt \
+--output_model_path /home/leo/lh/Projects/UER-py/models/roberta_wwm_ext_model.bin
