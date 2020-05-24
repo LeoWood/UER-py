2128e8104a577fb56b5ad05014820544a5d7ee0d
LeoWood <463861936@qq.com>
2020-05-24 17:33:43 +0800

    .

---
+++ corpora/corpora_seg.py
@@ -0,0 +1,14 @@
+import pkuseg
+from tqdm import tqdm
+
+pku_seg = pkuseg.pkuseg(model_name="medicine")
+
+with open('R_seg.txt','w',encoding='utf-8') as fw:
+    with open('R.txt','r',encoding='utf-8') as f:
+        for line in tqdm(f.readlines()):
+            line = line.strip()
+            if line:
+                [fw.write(word + ' ') for word in pku_seg.cut(line)]
+                fw.write('\n')
+
+
