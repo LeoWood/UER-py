b9f2647cc02b46172aa5ba5eb56d0d986270d1d5
LeoWood <463861936@qq.com>
2020-05-24 21:44:13 +0800

    .

--- scripts/vocab_cscd_r.cmd
+++ scripts/vocab_cscd_r.cmd
@@ -1,6 +1,6 @@
 python build_vocab.py ^
---corpus_path corpora/R_seg.txt ^
---vocab_path corpora/cscd_r_vocab.txt ^
+--corpus_path ../corpora/R_seg.txt ^
+--vocab_path ../corpora/cscd_r_vocab.txt ^
 --worker 4 ^
 --min_count 100
 
