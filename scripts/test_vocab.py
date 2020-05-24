0a249c6c0986df077730bb71646cd3f2af81bd41
LeoWood <463861936@qq.com>
2020-05-24 22:34:58 +0800

    .

--- scripts/test_vocab.py
+++ scripts/test_vocab.py
@@ -9,31 +9,34 @@ import os
 uer_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(uer_dir)
 
-from uer.utils.vocab import Vocab
-from uer.utils.tokenizer import WordpieceTokenizer
 from uer.utils.tokenizer import BertTokenizer
 import argparse
 
 
 
 if __name__ == '__main__':
-    vocab = Vocab()
-    print("input vocab path:")
-    vocab_path = input()
-    vocab.load(vocab_path)
-    tokenizer = WordpieceTokenizer(vocab)
 
+    vocab_path = r"E:\LiuHuan\Projects\UER-py\models\google_zh_vocab.txt"
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--vocab_path",default=vocab_path)
     args = parser.parse_args()
 
-    tokenizer_bert = BertTokenizer(args)
+    tokenizer_1 = BertTokenizer(args)
+
+    args.vocab_path = r"E:\LiuHuan\Projects\UER-py\corpora\cscd_r_vocab.txt"
+    tokenizer_2 = BertTokenizer(args)
+
+    args.vocab_path = r"E:\LiuHuan\Projects\bert-vocab-builder\cscd_r_vocab.txt"
+    tokenizer_3 = BertTokenizer(args)
+
     while True:
         text = input()
         text = text.lower()
-        print("wordpiece tokens:")
-        print(tokenizer.tokenize(text))
-        print("bert tokens:")
-        print(tokenizer_bert.tokenize(text))
+        print("google_zh_vocab")
+        print(tokenizer_1.tokenize(text))
+        print("uer_create_vocab:")
+        print(tokenizer_2.tokenize(text))
+        print("google_create_vocab:")
+        print(tokenizer_3.tokenize(text))
 
 
