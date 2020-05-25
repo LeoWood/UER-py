699d9ed6a65818d294c55cb19d249fc1d2fc5ae9
LeoWood <463861936@qq.com>
2020-05-25 16:56:38 +0800

    .

--- runs/pre_process_R.sh
+++ runs/pre_process_R.sh
@@ -2,7 +2,7 @@
 python ../preprocess.py \
 --corpus_path ../corpora/R.txt \
 --vocab_path ../models/google_zh_vocab.txt \
---dataset_path ../dataset/cscd_r_512.pt \
+--dataset_path cscd_r_512.pt \
 --seq_length 512 \
 --processes_num 4 \
 --target mlm
