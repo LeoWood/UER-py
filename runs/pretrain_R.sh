81f0bc20fa56213dbbccf69d3044e45f6c36d599
LeoWood <463861936@qq.com>
2020-05-25 21:30:10 +0800

    .

--- runs/pretrain_R.sh
+++ runs/pretrain_R.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-python pretrain.py \
---dataset_path cscd_r.pt \
---vocab_path models/google_zh_vocab.txt \
---pretrained_model_path models/cscd_R_based_on_google_zh-700000.bin \
+python ../pretrain.py \
+--dataset_path cscd_r_512.pt \
+--vocab_path ../models/google_zh_vocab.txt \
+--pretrained_model_path ../models/cscd_R_based_on_google_zh_70w+best.bin \
 --output_model_path models/cscd_R_based_on_google_zh_700000+.bin  \
 --output_log_path models/cscd_R_based_on_google_zh_700000-800000.csv  \
 --world_size 2 \
--- runs/pretrain_R_mlm_bert_base.sh
+++ runs/pretrain_R_mlm_bert_base.sh
@@ -1,10 +1,10 @@
 export PYTHONUNBUFFERED=1
 python ../pretrain.py \
---dataset_path ../cscd_r.pt \
+--dataset_path cscd_r_512.pt \
 --vocab_path ../models/google_zh_vocab.txt \
---pretrained_model_path ../models/cscd_R_based_on_google_zh-700000.bin \
---output_model_path ../models/cscd_R_based_on_google_zh_70w+.bin  \
---output_log_path ../models/cscd_R_based_on_google_zh_70w+.csv  \
+--pretrained_model_path ../models/cscd_R_based_on_google_zh_70w+best.bin \
+--output_model_path ../models/cscd_R_based_on_google_zh_512.bin  \
+--output_log_path ../models/cscd_R_based_on_google_zh_512.csv  \
 --world_size 2 \
 --gpu_ranks 0 1 \
 --total_steps 1000000 \
