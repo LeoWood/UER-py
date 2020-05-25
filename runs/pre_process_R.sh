3654954c57bfb16e3ba7fb415d01ff8db588b238
LeoWood <463861936@qq.com>
2020-05-25 15:35:06 +0800

    .

---
+++ runs/pre_process_R.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+python ../preprocess.py \
+--corpus_path ../corpora/R.txt \
+--vocab_path ../models/google_zh_vocab.txt \
+--dataset_path ../dataset/cscd_r_512.pt \
+--seq_length 512 \
+--processes_num 4 \
+--target mlm
--- runs/pre_process_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-python preprocess.py \
---corpus_path corpora/book_review_bert.txt \
---vocab_path models/google_zh_vocab.txt \
---dataset_path dataset/book_review_bert.pt \
---processes_num 1 \
---target csci_mlm
