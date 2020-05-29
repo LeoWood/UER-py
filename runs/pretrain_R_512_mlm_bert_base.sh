a58e9c13960e0f2f3ee53e7b1588784e1f93c591
LeoWood <463861936@qq.com>
2020-05-29 20:42:47 +0800

    .

---
+++ runs/pretrain_R_512_mlm_bert_base.sh
@@ -0,0 +1,17 @@
+export PYTHONUNBUFFERED=1
+python ../pretrain.py \
+--dataset_path ../cscd_r.pt \
+--vocab_path ../models/google_zh_vocab.txt \
+--pretrained_model_path ../models/cscd_R_based_on_google_zh_60w+-best.bin \
+--output_model_path ../models/cscd_R_based_on_google_zh_512.bin  \
+--output_log_path ../models/cscd_R_based_on_google_zh_512.csv  \
+--world_size 2 \
+--gpu_ranks 0 1 \
+--total_steps 2000000 \
+--save_checkpoint_steps 100000 \
+--encoder bert \
+--batch_size 8 \
+--embedding bert \
+--target mlm \
+--add_pos 0 \
+--add_term 0
