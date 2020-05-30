974f76dd942593ed69a503bcc697ef2d53475131
LeoWood <463861936@qq.com>
2020-05-30 11:42:41 +0800

    .

--- runs/pre_process_R_512.sh
+++ runs/pre_process_R_512.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 python ../preprocess.py \
---corpus_path ../corpora/R.txt \
+--corpus_path ../corpora/pre_training_R_all.txt \
 --vocab_path ../models/google_zh_vocab.txt \
---dataset_path cscd_r_512.pt \
+--dataset_path ../wanfang_r_512.pt \
 --seq_length 512 \
 --processes_num 4 \
 --target mlm
