b29c71cc88260534452eefade6f66dce1e7f42a4
LeoWood <463861936@qq.com>
2020-06-02 23:34:33 +0800

    baidu

--- runs/pre_process_r_baidu.sh
+++ runs/pre_process_r_baidu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python ../preprocess_baidu.py \
---corpus_path ../corpora/R.txt \
+--corpus_path ../../data/data38224/R.txt \
 --vocab_path ../models/google_zh_vocab.txt \
 --dataset_path ../cscd_r.pt \
 --seq_length 128 \
