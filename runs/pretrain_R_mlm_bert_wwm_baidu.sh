8bd10bd78cb744aa537f79a95d12227e1c3d8155
LeoWood <463861936@qq.com>
2020-06-03 12:00:32 +0800

    .

--- runs/pretrain_R_mlm_bert_wwm_baidu.sh
+++ runs/pretrain_R_mlm_bert_wwm_baidu.sh
@@ -10,7 +10,7 @@ python ../pretrain_baidu.py \
 --total_steps 2000000 \
 --save_checkpoint_steps 100000 \
 --encoder bert \
---batch_size 128 \
+--batch_size 192 \
 --embedding bert \
 --target mlm \
 --add_pos 0 \
