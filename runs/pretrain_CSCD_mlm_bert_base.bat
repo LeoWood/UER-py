85ef58776b6b5b0c19b14172b9d6fb5eb1e0beea
LeoWood <463861936@qq.com>
2020-06-08 17:23:10 +0800

    .

--- runs/pretrain_CSCD_mlm_bert_base.bat
+++ runs/pretrain_CSCD_mlm_bert_base.bat
@@ -1,13 +1,13 @@
 python ../pretrain.py ^
 --dataset_path ../cscd.pt ^
 --vocab_path ../models/google_zh_vocab.txt ^
---pretrained_model_path ../models/google_zh_model.bin ^
---output_model_path ../output_pre/pre_cscd_from_bert_base.bin  ^
---output_log_path ../output_pre/pre_cscd_from_bert_base.csv  ^
+--pretrained_model_path ../output_pre/pre_cscd_from_bert_base-20w.bin ^
+--output_model_path ../output_pre/pre_cscd_from_bert_base_20w+.bin  ^
+--output_log_path ../output_pre/pre_cscd_from_bert_base_20w+.csv  ^
 --world_size 1 ^
 --gpu_ranks 1 ^
 --total_steps 5000000 ^
---save_checkpoint_steps 10000 ^
+--save_checkpoint_steps 50000 ^
 --encoder bert ^
 --batch_size 45 ^
 --embedding bert ^
