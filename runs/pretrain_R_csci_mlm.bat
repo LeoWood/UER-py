e019bda640861be009187aa817deb5f428614c5c
LeoWood <463861936@qq.com>
2020-06-18 13:07:03 +0800

    .

--- runs/pretrain_R_csci_mlm.bat
+++ runs/pretrain_R_csci_mlm.bat
@@ -1,12 +1,12 @@
 python ../pretrain.py ^
 --dataset_path ../cscd_r_csci_mlm.pt ^
 --vocab_path ../models/google_zh_vocab.txt ^
---pretrained_model_path ../output_pre/cscd_R_csci_mlm_based_on_google_zh_310w.bin ^
---output_model_path ../output_pre/cscd_R_csci_mlm_based_on_google_zh_310w+.bin  ^
---output_log_path ../output_pre/cscd_R_csci_mlm_310w+.csv  ^
+--pretrained_model_path ../output_pre/cscd_R_csci_mlm_based_on_google_zh_410w.bin ^
+--output_model_path ../output_pre/cscd_R_csci_mlm_based_on_google_zh_410w+.bin  ^
+--output_log_path ../output_pre/cscd_R_csci_mlm_410w+.csv  ^
 --world_size 1 ^
 --gpu_ranks 1 ^
---total_steps 1000000 ^
+--total_steps 5000000 ^
 --save_checkpoint_steps 10000 ^
 --encoder bert ^
 --batch_size 40 ^
