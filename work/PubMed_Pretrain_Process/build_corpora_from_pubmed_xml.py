682e696666b1906682bf674ae5c76fa92cd1f58f
LeoWood <463861936@qq.com>
2020-12-18 12:21:56 +0800

    .

--- work/PubMed_Pretrain_Process/build_corpora_from_pubmed_xml.py
+++ work/PubMed_Pretrain_Process/build_corpora_from_pubmed_xml.py
@@ -28,15 +28,16 @@ with open('pubmed_oa_noncm.txt','w',encoding='utf-8') as f:
     # 遍历整个文件夹，查找nxml文件
     for path,dir_list,file_list in os.walk(path):  
         for file_name in tqdm(file_list):
-            file_count += 1
-            file = os.path.join(path, file_name)
-            dicts_out = pp.parse_pubmed_paragraph(file, all_paragraph=False)
-            for dict in dicts_out:
-                para = dict['text']
-                sens = seg_sens(para)
-                for sen in sens:
-                    f.write(sen+'\n')
-                f.write('\n')
+            if '.nxml' in file_name:
+                file_count += 1
+                file = os.path.join(path, file_name)
+                dicts_out = pp.parse_pubmed_paragraph(file, all_paragraph=False)
+                for dict in dicts_out:
+                    para = dict['text']
+                    sens = seg_sens(para)
+                    for sen in sens:
+                        f.write(sen+'\n')
+                    f.write('\n')
 print(file_count)
 
 if __name__ == '__main__':
