a67924762f30f71c84d1b7e0e0616d9e258dc7fe
LeoWood <463861936@qq.com>
2020-12-22 16:24:04 +0800

    .

--- work/PubMed_Pretrain_Process/build_corpora_from_pubmed_xml.py
+++ work/PubMed_Pretrain_Process/build_corpora_from_pubmed_xml.py
@@ -12,40 +12,32 @@ from Seg_Sents_En import seg_sens
 # nlp = spacy.load("en_core_sci_sm")
 from tqdm import tqdm
 
-path = r'F:\PubMed全文'
+path = r'/data/PubMed_articles'
 file_count = 0
 files = []
-#
-with open('pubmed_oa_noncm.txt','w',encoding='utf-8') as f:
-    # 遍历整个文件夹，查找nxml文件
-    for path,dir_list,file_list in os.walk(path):
-        for file_name in file_list:
-            if '.nxml' in file_name:
-                file_count += 1
-                files.append(os.path.join(path, file_name))
+
+
+# 遍历整个文件夹，查找nxml文件
+for path,dir_list,file_list in os.walk(path):
+    for file_name in file_list:
+        if '.nxml' in file_name:
+            file_count += 1
+            files.append(os.path.join(path, file_name))
 print(file_count)
-# exit()
-#
-# path = ''
-# file_count = 0
 
 
-with open('pubmed_oa_noncm.txt','w',encoding='utf-8') as f:
+with open('corpora/pubmed_oa_noncm.txt','w',encoding='utf-8') as f:
     # 遍历整个文件夹，查找nxml文件
-    for file in tqdm(files):
+    for file in tqdm(files[:1000]):
         dicts_out = pp.parse_pubmed_paragraph(file, all_paragraph=False)
-        for dict in dicts_out:
-            para = dict['text']
-            try:
-                sens = seg_sens(para)
-                # sens = [str(sen) for sen in nlp(para).sents]
-            except:
-                print(para)
-                continue
-
-            for sen in sens:
-                f.write(sen+'\n')
-            f.write('\n')
+        if dicts_out:
+            for dict in dicts_out:
+                para = dict['text'].strip()
+                if para:
+                    sens = seg_sens(para)
+                    for sen in sens:
+                        f.write(sen+'\n')
+                    f.write(para+'\n')
 
 if __name__ == '__main__':
     pass
