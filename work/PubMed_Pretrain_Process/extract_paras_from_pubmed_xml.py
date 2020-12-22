ebf4692121ca153bbf8df7aea800e3a2987bdb98
LeoWood <463861936@qq.com>
2020-12-22 15:41:15 +0800

    .

--- work/PubMed_Pretrain_Process/extract_paras_from_pubmed_xml.py
+++ work/PubMed_Pretrain_Process/extract_paras_from_pubmed_xml.py
@@ -11,13 +11,16 @@ import os
 # import spacy
 # nlp = spacy.load("en_core_sci_sm")
 from tqdm import tqdm
+import os
+import sys 
+os.chdir(sys.path[0])
 
-path = r'F:\PubMed全文'
+path = r'/data/PubMed_articles'
 file_count = 0
 files = []
-#
 
-    # 遍历整个文件夹，查找nxml文件
+
+# 遍历整个文件夹，查找nxml文件
 for path,dir_list,file_list in os.walk(path):
     for file_name in file_list:
         if '.nxml' in file_name:
@@ -28,14 +31,12 @@ for path,dir_list,file_list in os.walk(path):
             with open('pubmed_oa_paras.txt','w',encoding='utf-8') as f:
                 dicts_out = pp.parse_pubmed_paragraph(file, all_paragraph=False)
                 if dicts_out:
-                    print(dicts_out)
                     for dict in dicts_out:
+                        print(dict)
                         para = dict['text']
                         f.write(para+'\n')
                     f.write('\n')
                     exit()
-
-
 
 print(file_count)
 # exit()
