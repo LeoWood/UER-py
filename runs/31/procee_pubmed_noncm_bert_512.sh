0854cd7b6e76affefdb35d5fc32e51bbe9e41375
LeoWood <463861936@qq.com>
2020-12-23 16:03:09 +0800

    .

---
+++ runs/31/procee_pubmed_noncm_bert_512.sh
@@ -0,0 +1,7 @@
+python ../../preprocess.py \
+--corpus_path ../../corpora/pubmed_oa_noncm.txt \
+--vocab_path ../../models/google_cased_en_vocab.txt \
+--dataset_path ../../corpora/pubmed_oa_noncm.pt \
+--seq_length 512 \
+--processes_num 16 \
+--target bert
